3538a7564a47760fa6768ce0e610511317f14898
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-01-29 14:52:03 -0300

    edited repo file

--- src/services/players-service.ts
+++ src/services/players-service.ts
@@ -1,7 +1,8 @@
+import * as PlayerRespository from "../repositories/players-repository";
 import { noContent, ok } from "../utils/http-helper";
 
 export const getPlayerService = async () => {
-  const data = null;
+  const data = await PlayerRespository.findAllPlayers();
   let response = null;
 
   if (data) {
