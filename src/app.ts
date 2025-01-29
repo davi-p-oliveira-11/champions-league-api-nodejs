f1eee907a7c4d897b98a96c3e2a48a524221b2be
davi-p-oliveira-11 <dpo.dev11@gmail.com>
2025-01-28 21:28:59 -0300

    added player-controller folder

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,12 @@
 import express, { Request, Response } from "express";
+import { getPlayer } from "./controllers/player-controller";
 
 function createApp() {
   const app = express();
 
   app.use(express.json());
 
-  app.get("/", (req: Request, res: Response) => {
-    res.status(200).json({ player: "messi" });
-  });
+  app.get("/", getPlayer);
 
   return app;
 }
